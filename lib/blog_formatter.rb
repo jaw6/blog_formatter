6c0e38678bb85727642164cbd6068a195a57b7a7
Joshua Wehner <joshua.wehner@gmail.com>
2012-12-05 16:55:19 -0600

    Pasted from old app - this almost works

--- lib/blog_formatter.rb
+++ lib/blog_formatter.rb
@@ -1,2 +1,65 @@
 module BlogFormatter
+  extend self
+  
+  def close_tags(code)
+    open_tags = {}
+    closed_tags = {}
+    code.scan(/\<([^\>\s\/]+)[^\>\/]*?\>/).each { |t| open_tags[t[0]] ? open_tags[t[0]] += 1 : open_tags[t[0]] = 1 }
+    code.scan(/\<\/([^\>\s\/]+)[^\>]*?\>/).each { |t| closed_tags[t[0]] ? closed_tags[t[0]] += 1 : closed_tags[t[0]] = 1 }
+    open_tags.each {|k,v| code += "</#{k}>" * (open_tags[k] - closed_tags[k].to_i) if closed_tags[k].to_i < v }
+    return code
+  end
+  
+  def limited_html(string, limit)
+    counter = 0
+    new_string = ''
+    stopped = false
+    string.split(/>/).each do |chunk|
+      if counter >= limit
+        stopped = true
+        break
+      end
+      if chunk =~ /^</ # if this is a closing tag, just close it
+        new_string << chunk + '>'
+      else
+        nontagpart = chunk.split(/</).first
+        # if the non-tag portion of the chunk is smaller than the remaining limit, just add it
+        if (counter + nontagpart.size) < limit 
+          counter += nontagpart.size
+          new_string << chunk + '>'
+        else
+          potential = ''
+          for word in nontagpart.split(/\s/)
+            if counter < limit 
+              counter += word.size + 1
+              potential << word + ' '
+            else
+              stopped = true
+              # don't add this, unless it's meaty
+              new_string << potential unless potential.size < 10 
+              break
+            end
+          end
+        end
+      end
+    end
+    new_string = new_string.gsub(/[,.;-]\s*$/, ' ').gsub(/\s+/, ' ') # remove trailing punctuation & excess whitespace
+    new_string = new_string.gsub(%r{<[^/]+>$}, '') # remove the last open tag if any
+    new_string += '&hellip;' if stopped # add an ellipsis if we stopped early
+    close_tags(new_string)
+  end
+  
+  def no_paras(string)
+    string = string[3..-1] if string[0..2] == '<p>'
+    string = string[0..-5] if string[-4..-1] == '</p>'
+    string
+  end
+  
+  def truncate_html(string, limit=150)
+    string = string.strip
+    return string if string.size <= limit
+    limited = limited_html(string, limit)
+    return limited
+  end
+  
 end
--- test/blog_formatter_test.rb
+++ test/blog_formatter_test.rb
@@ -4,4 +4,78 @@ class BlogFormatterTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, BlogFormatter
   end
+
+  def setup
+    @strings = []
+    @strings << <<-EOT
+    <p>This one is easy. It's super short.</p>
+    EOT
+    @strings << <<-EOT
+    <p>War will then confront me. I think of others by which this tariff taxation shall be diverted from this time existing in other improvements of agriculture, commerce, and navigation. I have used the words "they" and "their" in speaking of a war which might be affected.</p>
+    EOT
+    @strings << <<-EOT
+    <p>War will then confront me. I think of others by which this tariff taxation shall be diverted from this time existing in other <strong>improvements of agriculture, commerce, and navigation. I have used the</strong> words "they" and "their" in speaking of a war which might be affected.</p>
+    EOT
+    @strings << <<-EOT
+    <p>A group, <a href="http://www.pnhp.org">Physicians for a National Health Program</a>, has an interesting <a href="http://www.pnhp.org/news/2008/january/doctors_give_massach.php">press release</a>, distilled from an &#8220;open letter&#8221; signed by 250 doctors in Massachussets. (I learned about it from <a href="http://www.cjr.org/campaign_desk/mitts_health_plan_debunked.php?page=all"><span class="caps">CJR</span></a>, which says that it&#8217;s not getting any media coverage.)</p>
+
+    <p>The ostensible goal of the program is to &#8220;provide residents with medical insurance&#8221;, that is, by <strong>requiring</strong> them to pay for health insurance. If they don&#8217;t, they will be fined. If they&#8217;re a certain distance below the poverty line, there&#8217;s a tax refund for the insurance. Employers who don&#8217;t provide health insurance to their employees are also fined.</p>
+    EOT
+    
+    @strings << <<-EOT
+    <p>The &#8220;4 Things&#8221; meme:</p>
+
+    <h4>Jobs I Have Had in My Life</h4>
+  	<ol>
+    	<li>Web Developer</li>
+  		<li>Temp</li>
+  		<li>Teaching Assistant</li>
+  		<li>Baby-sitter</li>
+  	</ol>
+
+  	<h4>Movies Seen More Than Once</h4>
+  	<ol>
+    	<li>Shaun of the Dead</li>
+  		<li>Idiocracy</li>
+  		<li>Star Wars</li>
+  		<li>The Incredibles</li>
+  	</ol>
+    EOT
+  end
+  
+  def test_truncate_short_should_just_return
+    string = @strings[0]
+    assert_equal "<p>This one is easy. It's super short.</p>", BlogFormatter.truncate_html(string)
+  end
+  
+  def test_truncate_longer_should_end_with_ellisp
+    string = @strings[1]
+    assert_equal "<p>War will then confront me. I think of others by which this tariff taxation shall be diverted from this time existing in other improvements of agriculture &hellip;</p>", BlogFormatter.truncate_html(string)
+  end
+  
+  def test_truncate_should_rejoin_tags_that_get_split
+    string = @strings[2]
+    assert_equal "<p>War will then confront me. I think of others by which this tariff taxation shall be diverted from this time existing in other <strong>improvements of agriculture &hellip;</strong></p>", BlogFormatter.truncate_html(string)
+  end
+  
+  def test_truncate_should_remove_empty_tags
+    string = @strings[3]
+    assert_equal '<p>A group, <a href="http://www.pnhp.org">Physicians for a National Health Program</a>, has an interesting <a href="http://www.pnhp.org/news/2008/january/doctors_give_massach.php">press release</a>, distilled from an &#8220;open letter&#8221; signed by 250 doctors &hellip;</p>', BlogFormatter.truncate_html(string)
+  end
+  
+  def test_truncate_should_remove_empty_tags_longer
+    string = @strings[3]
+    assert_equal '<p>A group, <a href="http://www.pnhp.org">Physicians for a National Health Program</a>, has an interesting <a href="http://www.pnhp.org/news/2008/january/doctors_give_massach.php">press release</a>, distilled from an &#8220;open letter&#8221; signed by 250 doctors in Massachussets. (I learned &hellip;</p>', BlogFormatter.truncate_html(string, 180)
+  end
+  
+  def test_truncate_placement_of_ellipsis_in_funny_ending_blocks
+    string = @strings[4]
+    assert_equal '<p>The &#8220;4 Things&#8221; meme:</p> <h4>Jobs I Have Had in My Life</h4> <ol> <li>Web Developer</li> <li>Temp</li> <li>Teaching Assistant</li> <li>Baby-sitter</li> </ol> <h4>Movies Seen More &hellip;</h4>', BlogFormatter.truncate_html(string, 160)
+  end
+  
+  def test_truncate_should_not_include_short_strings
+    string = @strings[4]
+    assert_equal '<p>The &#8220;4 Things&#8221; meme:</p> <h4>Jobs I Have Had in My Life</h4> <ol> <li>Web Developer</li> <li>Temp</li> <li>Teaching Assistant</li> <li>Baby-sitter</li> </ol> &hellip;', BlogFormatter.truncate_html(string)
+  end
+
 end
